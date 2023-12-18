30b7761bedcc00247f088ce5d710f59467455bc0
Richard Lucas <richardlucasfm@gmail.com>
2023-12-18 13:55:05 -0300

    at

--- src/app/workspace/layout.tsx
+++ src/app/workspace/layout.tsx
@@ -1,31 +1,70 @@
 "use client";
 
-import TabletMacIcon from '@mui/icons-material/TabletMac';
+import AssignmentIcon from '@mui/icons-material/Assignment';
+import BusinessIcon from '@mui/icons-material/Business';
+import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted';
+import FolderOpenIcon from '@mui/icons-material/FolderOpen';
+import GroupsIcon from '@mui/icons-material/Groups';
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import MenuIcon from '@mui/icons-material/Menu';
-import { Box, Drawer, IconButton, List, ListItem, ListItemButton, ListItemIcon, Toolbar, Typography } from "@mui/material"
+import { Box, Container, Drawer, IconButton, List, ListItem, ListItemButton, ListItemIcon, Toolbar, Typography } from "@mui/material"
 import { ReactNode, useState } from "react"
+import { IDrawerMenu } from '../interfaces/workspace';
+import styles from './styles.module.css';
 
-const drawerWidth = 240;
+const drawerWidth = 200;
 
 export default function Layout({ children }: { children: ReactNode }) {
     const [mobileOpen, setMobileOpen] = useState(false);
+    const pathname = usePathname();
 
     const handleDrawerToggle = () => {
         setMobileOpen((old_state) => !old_state);
     };
 
+    const drawer_menus: IDrawerMenu[] = [
+        {
+            label: 'Workspace',
+            icon: <BusinessIcon />,
+            link: '/workspace/home'
+        },
+        {
+            label: 'Briefings',
+            icon: <AssignmentIcon />,
+            link: '/workspace/briefings'
+        },
+        {
+            label: 'Clientes',
+            icon: <GroupsIcon />,
+            link: '/workspace/clients'
+        },
+        {
+            label: 'Catálogos',
+            icon: <FormatListBulletedIcon />,
+            link: '/workspace/catalogs'
+        },
+        {
+            label: 'Portfolios',
+            icon: <FolderOpenIcon />,
+            link: '/workspace/portfolios'
+        }
+    ];
+
     const drawer = (
-        <List>
-            <ListItem>
-                <ListItemButton>
-                    <ListItemIcon>
-                        <TabletMacIcon />
-                    </ListItemIcon>
-                    <Typography>
-                        Workspace
-                    </Typography>
-                </ListItemButton>
-            </ListItem>
+        <List sx={{ marginTop: { sm: '96px' } }}>
+            {drawer_menus.map((item: IDrawerMenu, index: number) => (
+                <Link href={item.link} key={index} className={[styles.blackLink, (pathname === item.link ? styles.activeLink : '')].join(' ')}>
+                    <ListItem>
+                        <ListItemButton>
+                            {item.icon}
+                            <Typography sx={{ marginLeft: '10px' }}>
+                                {item.label}
+                            </Typography>
+                        </ListItemButton>
+                    </ListItem>
+                </Link>
+            ))}            
         </List>
     );
 
@@ -36,7 +75,6 @@ export default function Layout({ children }: { children: ReactNode }) {
                 sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
                 aria-label="mailbox folders"
             >
-                {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
                 <Drawer
                     variant="temporary"
                     open={mobileOpen}
@@ -46,7 +84,11 @@ export default function Layout({ children }: { children: ReactNode }) {
                     }}
                     sx={{
                         display: { xs: 'block', sm: 'none' },
-                        '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+                        '& .MuiDrawer-paper': {
+                            backgroundColor: "#f0f0f0",
+                            boxSizing: 'border-box',
+                            width: drawerWidth 
+                        },
                     }}
                 >
                     {drawer}
@@ -55,7 +97,12 @@ export default function Layout({ children }: { children: ReactNode }) {
                     variant="permanent"
                     sx={{
                         display: { xs: 'none', sm: 'block' },
-                        '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+                        '& .MuiDrawer-paper': {
+                            backgroundColor: '#f0f0f0',
+                            boxSizing: 'border-box',
+                            width: drawerWidth,
+                            borderRight: 'none'
+                        },
                     }}
                     open
                 >
@@ -77,7 +124,9 @@ export default function Layout({ children }: { children: ReactNode }) {
                         Bem-vindo, Marfadomal
                     </Typography>
                 </Toolbar>
-                {children}
+                <Container>
+                    {children}
+                </Container>
             </Box>            
         </Box>
     )
